1ba3744565813585eeb5f628ea5aeec765f45c95
Ganesh553 <72643633+Ganesh553@users.noreply.github.com>
2021-01-11 07:43:55 +0530

    Update SnakeAndLadder.js

---
+++ SnakeAndLadder.js
@@ -0,0 +1,13 @@
+class Snakeladder{
+    DiceFunction(){
+        var WinPoint=100;
+        var Ladder=1;
+        var Snake=2;
+        var NoPlay=0;
+        var Player=0;
+        var diceCount=0;        
+        console.log("Player Starts play")
+    }
+}
+let ref = new Snakeladder();
+ref.DiceFunction()
